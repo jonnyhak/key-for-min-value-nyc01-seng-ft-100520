e6c1e071840fa57f1d44b022901dd08ada95760e
Jonathan Hakimian <jonnyhak@gmail.com>
2020-09-15 14:27:24 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,8 +5,8 @@ def key_for_min_value(name_hash)
   min = name_hash
   min_name = nil 
   name_hash.each do |name, value|
-    min = value if value < min
     min_name = name if value < min
+    min = value if value < min
   end
   min_name
 end
